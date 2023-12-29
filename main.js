9f21f1f51821d1fe4ebf94a774e9460206f26102
SOFT RAY <145141947+Raymondconcept12@users.noreply.github.com>
2023-12-29 13:39:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -12,37 +12,103 @@ let questions = [
     }, 
 
     {
-        question: "Which of these is not an Africa country?",
+        question: "From which language is the word 'ketchup' derived?",
         answer: [
 
-            { text: "Nepal", correct: true},
-            { text: "Congo", correct: false},
-            { text: "Ghana", correct: false},
-            { text: "South africa", correct: false}
+            { text: "Chinese", correct: true},
+            { text: "African", correct: false},
+            { text: "English", correct: false},
+            { text: "French", correct: false}
         ]
     }, 
 
     {
-        question: "Which is the largest animal in the world?",
+        question: "Which country has the biggest population in Europe?",
         answer: [
 
-            { text: "Shark", correct: false},
-            { text: "Blue whale", correct: true},
-            { text: "Dragon", correct: false},
-            { text: "Elephant", correct: false}
+            { text: "Nigeria", correct: false},
+            { text: "Russia", correct: true},
+            { text: "Italy", correct: false},
+            { text: "Estonia", correct: false}
         ]
     }, 
 
     {
-        question: "Which is the smallest continent in the world?",
+        question: "Who portrayed Edward Scissorhands?",
         answer: [
 
-            { text: "Asia", correct: false},
-            { text: "Arctic", correct: false},
-            { text: "Africa", correct: false},
-            { text: "Australia", correct: true}
+            { text: "Johnny Sin", correct: false},
+            { text: "Williams brat", correct: false},
+            { text: "John Cena", correct: false},
+            { text: "Johnny Depp", correct: true}
         ]
-    }
+    } ,
+
+    {
+        question: "What are made and repaired by a cobbler?",
+        answer: [
+
+            { text: "Umbrella", correct: false},
+            { text: "Shoe", correct: true},
+            { text: "Corps", correct: false},
+            { text: "Stone", correct: false}
+        ]
+    } ,
+
+    {
+        question: "What color are the four stars on the flag of New Zealand?",
+        answer: [
+
+            { text: "White", correct: false},
+            { text: "Blue", correct: false},
+            { text: "Green", correct: false},
+            { text: "Red", correct: true}
+        ]
+    } ,
+
+    {
+        question: "How many states make up the United States of America?",
+        answer: [
+
+            { text: "50", correct: true},
+            { text: "45", correct: false},
+            { text: "20", correct: false},
+            { text: "36", correct: false}
+        ]
+    } ,
+
+    {
+        question: "Which English king married six times?",
+        answer: [
+
+            { text: "John Lackland", correct: false},
+            { text: "Henry VIII", correct: true},
+            { text: "Edward VIII", correct: false},
+            { text: "Elizabeth I", correct: false}
+        ]
+    } ,
+
+    {
+        question: "H2O is the chemical formula for what?",
+        answer: [
+
+            { text: "Cement", correct: false},
+            { text: "Acid", correct: false},
+            { text: "Water", correct: true},
+            { text: "Sand", correct: false}
+        ]
+    } ,
+
+    {
+        question: "What has a face and two hands, but no arms or leg?",
+        answer: [
+
+            { text: "Tree", correct: false},
+            { text: "Humans", correct: false},
+            { text: "Snake", correct: false},
+            { text: "Clock", correct: true}
+        ]
+    } 
 
 
 ];
